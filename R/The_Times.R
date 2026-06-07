9fa91fe3be12e7be022dcf65d5956148ce773785
Viky Rackova <viki.rackova@gmail.com>
2026-06-07 23:41:57 +0200

    Initial package commit

--- R/The_Times.R
+++ R/The_Times.R
@@ -2,9 +2,9 @@
 #'
 #' A dataset containing articles published in \emph{The Times}
 #' that discuss government and public administration.
-#' The dataset covers the period from January 2020 to December 2025.
+#' The dataset covers the period from January 2020 to December 2020.
 #'
-#' @format A data frame with 179,760 observations and 2 variables:
+#' @format A data frame with 33,408 observations and 2 variables:
 #' \describe{
 #'   \item{Date}{The publication date of the article.}
 #'   \item{Text}{The full text of the article.}
